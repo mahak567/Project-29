2d1611bb1fa09d9ef21becec00b5c722c09a7b04
mahak567 <74048800+mahak567@users.noreply.github.com>
2021-01-27 20:14:37 +0530

    Add files via upload

---
+++ TowerSiege-1-main/Stump.js
@@ -0,0 +1,21 @@
+class Stump{
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic:true
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("stump.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(0,0,this.width, this.height);
+        pop();
+      }
+}
